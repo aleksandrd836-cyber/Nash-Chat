b2e17d46753ec33190f868dccc53e49995a60dfb
aleksandrd836-cyber <aleksandrd836-cyber@users.noreply.github.com>
2026-04-08 21:36:18 +0000

    deploy: 8bdde255ffe1723df84af34f8059004796a46944

--- assets/AuthPage-9EmBU1yV.js
+++ assets/AuthPage-9EmBU1yV.js
@@ -1,4 +1,4 @@
-import{c as n,r as a,j as e}from"./index-BCKsa_Kq.js";import{U as k,a as _}from"./user-D3rxFAxy.js";/**
+import{c as n,r as a,j as e}from"./index-CBhp_ZNV.js";import{U as k,a as _}from"./user-DID0N-GF.js";/**
  * @license lucide-react v0.400.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/ServerSettingsModal-Bf-BehC8.js
+++ assets/ServerSettingsModal-Bf-BehC8.js
@@ -1,4 +1,4 @@
-import{c as o,r,s as c,j as e,b as W,X,C as B,g as F}from"./index-BCKsa_Kq.js";import{U as G,a as J}from"./user-D3rxFAxy.js";/**
+import{c as o,r,s as c,j as e,b as W,X,C as B,g as F}from"./index-CBhp_ZNV.js";import{U as G,a as J}from"./user-DID0N-GF.js";/**
  * @license lucide-react v0.400.0 - ISC
  *
  * This source code is licensed under the ISC license.
