77af3b4d0718aad4d1c26522d14e04cbe44f4846
aleksandrd836-cyber <aleksandrd836-cyber@users.noreply.github.com>
2026-04-08 21:48:33 +0000

    deploy: dde45d06e5e4f90a41ab009cff80097cdeabbf43

--- assets/AuthPage-Br5eYUdq.js
+++ assets/AuthPage-Br5eYUdq.js
@@ -1,4 +1,4 @@
-import{c as n,r as a,j as e}from"./index-2XF1hKQZ.js";import{U as k,a as _}from"./user-wj6PzBOY.js";/**
+import{c as n,r as a,j as e}from"./index-CJgUAQT7.js";import{U as k,a as _}from"./user-BiXd7nCf.js";/**
  * @license lucide-react v0.400.0 - ISC
  *
  * This source code is licensed under the ISC license.
