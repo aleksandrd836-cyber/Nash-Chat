4d3a948236cb9298381094f7d0fe936d3c3aed62
aleksandrd836-cyber <aleksandrd836-cyber@users.noreply.github.com>
2026-04-08 21:53:23 +0000

    deploy: 7173ccf96f4fe48681cf1ba9455aa0d571e239b8

--- assets/ServerSettingsModal-VJv4E8cT.js
+++ assets/ServerSettingsModal-VJv4E8cT.js
@@ -1,4 +1,4 @@
-import{c as o,r,s as c,j as e,b as W,X,C as B,g as F}from"./index-CJgUAQT7.js";import{U as G,a as J}from"./user-BiXd7nCf.js";/**
+import{c as o,r,s as c,j as e,b as W,X,C as B,g as F}from"./index-2XF1hKQZ.js";import{U as G,a as J}from"./user-wj6PzBOY.js";/**
  * @license lucide-react v0.400.0 - ISC
  *
  * This source code is licensed under the ISC license.
