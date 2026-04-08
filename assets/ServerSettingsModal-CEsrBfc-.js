11328fd39e4bf62f2eee15d420eded792a7df6f0
aleksandrd836-cyber <aleksandrd836-cyber@users.noreply.github.com>
2026-04-08 21:08:54 +0000

    deploy: 7dd7022eb9ce1b8f22a793cb3ef47d08ef8c11e8

--- assets/ServerSettingsModal-CEsrBfc-.js
+++ assets/ServerSettingsModal-CEsrBfc-.js
@@ -1,4 +1,4 @@
-import{c as o,r,s as c,j as e,b as W,X,C as B,g as F}from"./index-7Hle9nbW.js";import{U as G,a as J}from"./user-BmCgBgx0.js";/**
+import{c as o,r,s as c,j as e,b as W,X,C as B,g as F}from"./index-DOZzdl21.js";import{U as G,a as J}from"./user-B2qDxQDi.js";/**
  * @license lucide-react v0.400.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/user-B2qDxQDi.js
+++ assets/user-B2qDxQDi.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-7Hle9nbW.js";/**
+import{c as e}from"./index-DOZzdl21.js";/**
  * @license lucide-react v0.400.0 - ISC
  *
  * This source code is licensed under the ISC license.
